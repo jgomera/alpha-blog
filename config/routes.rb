46da79c5cad75bf4ed80bdeb1f1acf1705fb12b7
JGomera <gomera81@gmail.com>
2018-03-04 18:44:51 -0400

    Updated root page -- JGomera

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#Esto me permite enrutar siempre al directorio raiz de mi pagina Solved by Jgomera
+  root 'pages#home'
 
-  #root 'pages#home'
-  get 'pages/home', to: 'pages#home'
-  #get 'about', to: 'root_path'
   get 'pages/about', to:'pages#about'
   #Nuevo link para crear articulos
   resources :articles
